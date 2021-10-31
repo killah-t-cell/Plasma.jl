a3e11d57fe28008c78ef8baf19a0e8e9bb6a4186
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-10-31 15:44:00 +0100

    started working on the abstract type interface

--- src/Plasma.jl
+++ src/Plasma.jl
@@ -9,9 +9,46 @@ import ModelingToolkit: Interval, infimum, supremum
 
 # interface for tokamak geometry (where you can pick tokamak parameters). 
 # Same for other common configurations – FRCs, Mirrors, Spheromaks... 
-struct Geometry{ T }
 
+
+# distribution.jl, plasma.jl, solve.jl, analyze.jl
+
+# abstract type AbstractObject end # so we can specify a wall that logs when a plasma hits it.
+
+abstract type AbstractCoil end
+ 
+abstract type AbstractPlasma end
+
+abstract type AbstractGeometry end
+
+abstract type AbstractDistribution end
+
+struct MaxwellianDistribution <: AbstractDistribution
 end
+
+
+struct Species
+    q::Float64
+    m::Float64
+    concentration::Float16 # defaults to 1
+end
+
+# given f(t,x,v) we can compute density, temperature, and all other parameters. It is probably wise to store T, and n, and use that as inputs to functions that compute all parameters.
+struct CollisionlessPlasma <: AbstractPlasma
+    temperature::Float64 # eV
+    density::Float64 # g/cc
+    species::Vector{Species}
+    initial_distributions::Vector{Distribution}
+    geometry::Geometry
+end
+
+# abstract type PlasmaGeometry <: Geometry end
+
+# abstract type ToroidalPlasmaGeometry <: PlasmaGeometry end
+
+
+
+
 
 #TODO learn about subtyping
 struct ToroidalGeometry{ T } # <: Geometry
