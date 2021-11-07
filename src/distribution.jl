ebbca1f1d72da145ea302eeaa07e9ec216a5d250
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-11-07 14:17:22 +0100

    finally happy with the interface. Examples look great. Solves are tested.

--- src/distribution.jl
+++ src/distribution.jl
@@ -1,28 +1,24 @@
-@with_kw struct Maxwellian{Ty, V} <: AbstractDistribution
-    v::V
-    v_drift::V = zeros(length(v))
-    T::Ty # Temperature in eV
-    m::Ty # Mass in Kg
-    P::Ty = nothing # probability distribution : 0 ≤ P ≤ 1
-
-    function Maxwellian(v, T, m; v_drift=zeros(length(v)))
-        if !(v isa Array)
-            v = [v]    
-        end
-
-        if length(v) != length(v_drift)
-            error("v and v_drift should have the same length")
-        end
-
-        v_ = sqrt(sum(v .^2))
-        v_drift_ = sqrt(sum(v_drift.^2))
+# TODO add Kappa and other relevant distributions
+function Maxwellian(T, m; v_drift=zeros())
         Kb = 8.617333262145e-5
         v_th = sqrt(2*Kb*T/m)
-        P = (π*v_th^2)^(-3/2) * exp(-(v_ - v_drift_)^2/v_th^2)
 
-        new{typeof(m),typeof(v)}(
-            v, v_drift, T, m, P
-        )
+        function P(x,v) 
+            if !(v isa Array)
+                v = [v]    
+            end
 
-    end
+            if v_drift == zeros()
+                v_drift = zeros(length(v))
+            end
+
+            if length(v) != length(v_drift)
+                error("v and v_drift should have the same length")
+            end
+
+            v_ = sqrt(sum(v .^2))
+            v_drift_ = sqrt(sum(v_drift.^2))
+
+            (π*v_th^2)^(-3/2) * exp(-(v_ - v_drift_)^2/v_th^2)
+        end
 end
