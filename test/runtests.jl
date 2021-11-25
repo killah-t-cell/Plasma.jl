0982950fc204fc7cba4d5ec0b1efcb699abbc741
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-11-25 10:08:40 +0100

    add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using Plasma
 using Test
 
 @testset "Plasma.jl" begin
-    # Write your tests here.
+    TD = 30000
+    D = species.D
+    D_D = Distribution(Maxwellian(TD, D.m), D) 
+    G = Geometry() 
+    plasma = ElectrostaticPlasma([D_D], G)
+
+    sol = Plasma.solve(plasma, dim=1, GPU=false, ub=0.1) 
+    @test sol isa PlasmaSolution
 end
