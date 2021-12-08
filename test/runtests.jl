aa69d70f9087f2e4338a598eda725a435e08e25a
Gabriel Birnbaum <gbbinbum@gmail.com>
2021-12-07 18:48:48 -0800

    trying to fix CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,4 @@
 using Pkg
 using SafeTestsets
 
-const GROUP = get(ENV, "GROUP", "All")
-
-@time begin
-  if GROUP == "All" || GROUP == "plasmatests"
-      @time @safetestset "Plasma tests" begin include("plasmatests.jl") end
-  end
-end
+@time @safetestset "Plasma tests" begin include("plasmatests.jl")
