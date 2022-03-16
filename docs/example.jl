653667ab164bf0b42264dc31b0af2d2df839c023
killah-t-cell <gbbinbum@gmail.com>
2022-03-16 17:33:50 +0000

    added a few plots

--- docs/example.jl
+++ docs/example.jl
@@ -1,61 +1,33 @@
 using Plasma
+using BSON: @save
 
-TD = 30000 # eV
-Te = 10000 # eV
+@info "starting two stream instability model"
 
-D = species.D
 e = species.e
 
-D_D = Distribution(Maxwellian(TD, D.m), D) 
-D_e = Distribution(Maxwellian(Te, e.m), e) 
-G = Geometry() 
+function TwoStream(vth2, vs1, vs2) 
 
-plasma = ElectrostaticPlasma([D_D, D_e], G)
+    function P(x,v)
+        if !(v isa Array)
+            v = [v]    
+        end
 
-sol = Plasma.solve(plasma, dim=1, GPU=false) 
+        if !(x isa Array)
+            x = [x]    
+        end
 
-Plasma.plot(sol)
+        v = sqrt(sum(v .^2))
+        x = sqrt(sum(x .^2))
 
-## 2D with custom P and species
-using Plasma
-
-Tα = 70000 # eV
-
-α = Species(1.602176634e-19, 6.6446562e-27)
-
-function HotCarrier(T) 
-    Kb = 8.617333262145e-5
-    P(x,v) = exp(-v/(Kb*T))
+        0.5/sqrt(vth2 * π) * exp(-(v-vs1)*(v-vs1)/vth2) + 0.5/sqrt(vth2 * π) * exp(-(v-vs2)*(v-vs2)/vth2) * (1+0.02*cos(3*π*x))
+    end
 end
 
-Dα = Distribution(HotCarrier(Tα), α)
-G = Geometry() # TODO define a custom geometry
+D_e = Distribution(TwoStream(0.02, 1.6, -1.4), e) 
+G = Geometry() 
 
-plasma = ElectrostaticPlasma([Dα], G)
+plasma = ElectrostaticPlasma([D_e], G)
 
-Plasma.solve(plasma, dim=2) # with GPU
+sol = Plasma.solve(plasma, dim=1, GPU=false, time_ub = 100.0, ub=2.0) 
 
-Plasma.plot(sol)
-
-
-## 3D CollisionlessPlasma
-using Plasma
-
-TD = 15000 # eV
-TT = 15000 # eV
-Te = 13000 # eV
-
-e = species.e
-T = species.T
-D = species.D
-
-De = Distribution(Maxwellian(Te, e.m), e)
-DT = Distribution(Maxwellian(TT, T.m), T)
-DD = Distribution(Maxwellian(TD, D.m), D)
-G = Geometry()
-
-plasma = CollisionlessPlasma([De,DT,DD], G)
-
-Plasma.solve(plasma)
-
-Plasma.plot(sol)
+@save "two_stream.bson" sol
